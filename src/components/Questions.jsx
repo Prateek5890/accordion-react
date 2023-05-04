55594e7a76b3a3e8734718e9970b11ac97433b10
Prateek5890 <prateekgangwar73@gmail.com>
2023-05-04 22:50:46 +0530

    first commit

---
+++ src/components/Questions.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+import Question from "./Question";
+const Questions = ({ questions, activeId, toggleQuestion }) => {
+  return (
+    <section className="container">
+      <h1>Questions</h1>
+      {questions.map((question) => (
+        <Question
+          key={question.id}
+          {...question}
+          activeId={activeId}
+          toggleQuestion={toggleQuestion}
+        />
+      ))}
+    </section>
+  );
+};
+
+export default Questions;
